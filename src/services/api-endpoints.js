9d8da60130c16953420a8f7880cdfdb86a22c7f0
Erik <erikwjonsson@gmail.com>
2018-09-08 14:52:32 +0200

    Created api client for consuming courses endpoint.

---
+++ src/services/api-endpoints.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://careerfoundry.com";
+
+const LANGUAGE = "/en";
+
+const COURSES = "/api/courses";
+const COURSE = "/api/course";
+
+const GET_COURSES_ENDPOINT = BASE_URL + LANGUAGE + COURSES;
+const GET_COURSE_ENDPOINT = BASE_URL + LANGUAGE + COURSE;
+
+export { GET_COURSES_ENDPOINT, GET_COURSE_ENDPOINT };
