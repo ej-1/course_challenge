165f99dcdb088e22c22eef156184afc50b985114
Erik <erikwjonsson@gmail.com>
2018-09-10 17:43:24 +0200

    Refactored CoursesTable to a functional componenet and created a mapper function.

--- src/components/lists/courses-table.js
+++ src/components/lists/courses-table.js
@@ -3,42 +3,27 @@ import CourseRow from "./course-row";
 import PropTypes from "prop-types";
 import "../lists/courses-table.css";
 
-// USE FUNCTIONAL COMPONENT INSTEAD
-class CoursesTable extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      select: []
-    };
-  }
-
-  render() {
-    // AM I SENDING IN TO MUCH DATA?
-    const courses = this.props.courses;
-    const coursesRows = [];
-    for (var course in this.props.courses) {
-      let courseData = courses[course];
-      coursesRows.push(
-        <CourseRow
-          courseData={courseData}
-          slug={course}
-          onSelect={this.props.onSelect}
-        />
-      );
-    }
-    // COULD EXTRACT TABLE INTO SEPARATE COMPONENT!!
-    return (
-      <table className="courses-table">
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Next Start Date</th>
-        </tr>
-        {coursesRows}
-      </table>
+const RowMapper = (courses, onSelect) => {
+  const coursesRows = [];
+  for (var course in courses) {
+    let courseData = courses[course];
+    coursesRows.push(
+      <CourseRow courseData={courseData} slug={course} onSelect={onSelect} />
     );
   }
-}
+  return coursesRows;
+};
+
+const CoursesTable = ({ courses, onSelect }) => (
+  <table className="courses-table">
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Next Start Date</th>
+    </tr>
+    {RowMapper(courses, onSelect)}
+  </table>
+);
 
 export default CoursesTable;
 
