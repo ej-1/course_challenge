693575c0de989059e72fd77995b37b81058899ab
Erik <erikwjonsson@gmail.com>
2018-09-11 17:19:13 +0200

    Made variables not global in tests.

--- src/components/lists/CourseRow.react.test.js
+++ src/components/lists/CourseRow.react.test.js
@@ -3,29 +3,32 @@ import CourseRow from "./CourseRow";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 
-const onSelect = jest.fn();
-// dont put in global scope
-const courseData = {
-  title: "How to write tests",
-  author: "Erik W. Jonsson",
-  next_start_formatted: "Monday, September 17th, 2018"
-};
-const slug = "/how-to-write-tests";
+describe("CourseRow", () => {
+  const courseData = {
+    title: "How to write tests",
+    author: "Erik W. Jonsson",
+    next_start_formatted: "Monday, September 17th, 2018"
+  };
 
-test("renders correctly", () => {
-  const tree = renderer
-    .create(
+  const slug = "/how-to-write-tests";
+
+  const onSelect = jest.fn();
+
+  test("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <CourseRow onSelect={onSelect} courseData={courseData} slug={slug} />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("onSelect sends slug", () => {
+    const wrapper = shallow(
       <CourseRow onSelect={onSelect} courseData={courseData} slug={slug} />
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
+    );
+    wrapper.find("tr").simulate("click");
 
-test("onSelect sends slug", () => {
-  const wrapper = shallow(
-    <CourseRow onSelect={onSelect} courseData={courseData} slug={slug} />
-  );
-  wrapper.find("tr").simulate("click");
-
-  expect(onSelect).toHaveBeenCalledWith("/how-to-write-tests");
+    expect(onSelect).toHaveBeenCalledWith("/how-to-write-tests");
+  });
 });
